1c970ed141aad006089ff0f9c4609348abab1a41
Anders Bohn Jespersen <abj@lenio.dk>
2013-07-06 19:30:25 +0200

    nye sider med engelsk tekst

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,42 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  def whyScala = Action {
+    Ok(views.html.why_scala())
+  }
+
+  def getStarted = Action {
+    Ok(views.html.get_started())
+  }
+
+  def howTos = Action {
+    Ok(views.html.howtos())
+  }
+
+  def howToMaven = Action {
+    Ok(views.html.howto_maven())
+  }
+
+  def links = Action {
+    Ok(views.html.links())
+  }
+
+  def scalaSceptics = Action {
+    Ok(views.html.sceptics())
+  }
+
+  def studyScala = Action {
+    Ok(views.html.study())
+  }
+  def funWithScala = Action {
+    Ok(views.html.fun_with())
+  }
+
 }
